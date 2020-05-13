ad1d7941dd814fe26b215982c0c0c07449ab91fa
nitish166 <knitish123@gmail.com>
2020-05-13 12:53:23 +0530

    Started Java Script

---
+++ FreeCodeCamp/JAVA Script/BasicOfJavaScript.js
@@ -0,0 +1,10 @@
+           /* Comment */
+
+// Comments of lines of code that java script intentionally ignore.
+
+// There are two ways to write commens in javaScript:
+
+// This is an in-line comment
+
+/* This is a multi-line comment */
+
