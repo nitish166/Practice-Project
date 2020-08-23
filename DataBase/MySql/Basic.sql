1567114280827f919e6aa9fcede65794db40442f
nitish166 <knitish123@gmail.com>
2020-08-23 20:06:58 +0530

    Database

--- DataBase/MySql/Basic.sql
+++ DataBase/MySql/Basic.sql
@@ -1,3 +1,13 @@
 # for creating data base schema syntax
 
 Create database testdb;
+
+# for create table
+
+create table Persons(
+ PersonID int,
+ LastName varchar(255),
+ FirstName varchar(255),
+ Address  varchar(255),
+ City varchar(255)
+);
