0983e77558e4960f96c04200aa12d5c4b10b139d
nitish166 <knitish123@gmail.com>
2020-10-06 21:38:43 +0530

    Function

--- JavaScript/Demo.js
+++ JavaScript/Demo.js
@@ -1,5 +1,13 @@
-
-for(let i = 0; i<=5; i++)
-{
-    console.log(i);
+// variable scope in function
+function message() {
+	//Local variable
+	var greet = "Hello JavaScript";
+	//Local variables are accessible n the function
+	console.log("Message from inside the function: " + greet);
 }
+message();
+//Local variable cannot be accessed outside the function
+console.log("Message from outside the function: " + greet);
+//Message from inside the function: Hello JavaScript
+//Uncaught ReferenceError: greet is not defined
+
