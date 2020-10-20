a39657f2125552d3a1625a769d20ebc22983a1d0
nitish166 <knitish123@gmail.com>
2020-10-20 22:20:40 +0530

    Adding event listener to a button

---
+++ JavaScript/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,13 @@
+
+// Adding event listeners to a button
+var numberOfDrumButton = document.querySelectorAll(".drum").length;
+for(var i=0; i<numberOfDrumButton; i++)
+{
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        alert("I got clicked");
+    });
+}
+
+
+
