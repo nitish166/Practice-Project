52bbf96a6a4bfdac5ab7f354b87aab68586b79ce
nitish166 <knitish123@gmail.com>
2020-10-21 22:40:58 +0530

    Higher order function

--- JavaScript/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -9,5 +9,32 @@ for(var i=0; i<numberOfDrumButton; i++)
     });
 }
 
+// higher order function
+
+function add(num1, num2)
+{
+    return num1+num2;
+}
+
+function substract(num1, num2)
+{
+    return num1-num2;
+}
+
+function multiply(num1, num2)
+{
+    return num1*num2;
+}
+
+function divide(num1, num2)
+{
+    return num1/num2;
+}
+
+function calculator(num1, num2, operator)
+{
+    return operator(num1, num2);
+}
+
 
 
