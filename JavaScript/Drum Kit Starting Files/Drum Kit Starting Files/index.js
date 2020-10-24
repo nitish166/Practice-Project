a4f9e50cff4b01a5fcfd018dc7e9e8ad5a78e82b
nitish166 <knitish123@gmail.com>
2020-10-24 21:22:36 +0530

    switch case

--- JavaScript/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ JavaScript/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -1,79 +1,51 @@
 
 // Adding event listeners to a button
 var numberOfDrumButton = document.querySelectorAll(".drum").length;
+
 for(var i=0; i<numberOfDrumButton; i++)
 {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        //alert("I got clicked");
-        // audio 
-        // var audio = new Audio('sounds/tom-1.mp3');
-        // audio.play();
-        console.log(this.innerHTML);
-        var temp = this.innerHTML;
-        if(temp =='w')
+
+
+        var buttonInnerHTML = this.innerHTML;
+        switch(buttonInnerHTML)
         {
-            var audio = new Audio('sounds/tom-1.mp3');
-            audio.play();
+            case 'w':
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+            case 'a':
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case 's':
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case 'd':
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            case 'j':
+                var crash = new Audio('sounds/crash.mp3');
+                crash.play();
+                break;
+            case 'k':
+                var kickBash = new Audio('sounds/kick-bass.mp3');
+                kickBash.play();
+                break;
+            case 'l':
+                var snare = new Audio('sounds/snare.mp3');
+                snare.play();
+                break;
+            default:
+                console.log(buttonInnerHTML);
         }
-        else if(temp == 'a')
-        {
-            var audio = new Audio('sounds/tom-2.mp3');
-            audio.play();
-        }
-        else if(temp == 's')
-        {
-            var audio = new Audio('sounds/tom-3.mp3');
-            audio.play();
-        }
-        else if(temp == 'd')
-        {
-            var audio = new Audio('sounds/tom-4.mp3');
-            audio.play();
-        }
-        else if(temp == 'j')
-        {
-            var audio  = new Audio('sounds/crash.mp3');
-            audio.play();
-        }
-        else if(temp == 'k')
-        {
-            var audio = new Audio('sounds/kick-bass.mp3')
-            audio.play();
-        }
-        else
-        {
-            var audio = new Audio('sounds/snare.mp3')
-            audio.play();
-        }
+        
     });
 }
 
-// higher order function
-
-// function add(num1, num2)
-// {
-//     return num1+num2;
-// }
-
-// function substract(num1, num2)
-// {
-//     return num1-num2;
-// }
-// function multiply(num1, num2)
-// {
-//     return num1*num2;
-// }
-
-// function divide(num1, num2)
-// {
-//     return num1/num2;
-// }
-
-// function calculator(num1, num2, operator)
-// {
-//     return operator(num1, num2);
-// }
 
 
 
